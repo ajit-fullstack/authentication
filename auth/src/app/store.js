345582002aaf067a83eedb202f91c5cf0e5c801f
Ajit pathak <ajitpathak0448@gmail.com>
2024-02-21 13:06:26 +0530

    commit

--- auth/src/app/store.js
+++ auth/src/app/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
-// import { pokemonApi } from './services/pokemon'
+import { userAuthApi } from '../services/userAuthApi'
 
 export const store = configureStore({
   reducer: {
-    // [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [userAuthApi.reducerPath]: userAuthApi.reducer,
     reducer: {}
   },
-//   middleware: (getDefaultMiddleware) =>
-    // getDefaultMiddleware().concat(pokemonApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userAuthApi.middleware),
 })
 
 setupListeners(store.dispatch)
