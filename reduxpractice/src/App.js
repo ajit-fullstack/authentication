b49ea634f2e7febcc379cf803bd83a2dff70b5c1
Ajit pathak <ajitpathak0448@gmail.com>
2024-03-05 15:10:00 +0530

    commit

--- reduxpractice/src/App.js
+++ reduxpractice/src/App.js
@@ -1,13 +1,27 @@
+import { createContext, useState } from 'react';
+import Child from './components/Child';
 import CreateTodo from './components/createTodo';
 import TodoList from './components/todoList';
 
+export const GlobalInfo = createContext()
+
 function App() {
+  const [color, setColor] = useState('green');
+
+  const getDay = (item) => {
+    console.log(item);
+  }
+  
   return (
-    <div className='createTodo'>
-      <CreateTodo />
-      <hr />
-      <TodoList />
-    </div>
+    <GlobalInfo.Provider value={{color: color, getDay: getDay}}>
+      <div className='createTodo'>
+        {/* <CreateTodo />
+        <hr />
+        <TodoList /> */}
+        <p>App components</p>
+        <Child />
+      </div>
+    </GlobalInfo.Provider>
   );
 }
 
